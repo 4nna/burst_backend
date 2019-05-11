d6f4f50bdb679873190ea1da07afc672dbb66105
Long Nguyen <longuyen@stud.hs-heilbronn.de>
2019-05-11 14:35:16 +0200

    Updated view

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
 
 from user.serializers import UserSerializer
@@ -10,6 +10,7 @@ User = get_user_model()
 class Register(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serialized = UserSerializer(data=request.DATA)
